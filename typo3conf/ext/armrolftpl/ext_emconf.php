c219d015953cce346588bd25200f7a12789b3c8a
Anisur <anisur.mullick@gmail.com>
2018-09-25 14:22:57 +0530

    [F] Starting template

---
+++ typo3conf/ext/armrolftpl/ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "armtemplate".
+ *
+ * Auto generated 06-06-2018 06:18
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'TYPO3 bootstrap template',
+	'description' => 'Template extension for rolf-benz 6.x',
+	'category' => 'templates',
+	'author' => 'Anisur R. Mullick',
+	'author_email' => 'anisur@armtechnologies.com',
+	'author_company' => 'ARM Technologies',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.1',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.0',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
---
+++ typo3conf/ext/armrolftpl/ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+$TCA['tt_content']['columns']['imagewidth']['config']['range']['upper'] = 1920;
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Rolf-Benz Template');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:armrolf/Configuration/TypoScript/TsConfig/pageTs.txt">');
